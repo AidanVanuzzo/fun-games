7a8e1ec8c35a6f25b0709ac096af497a72c7c1bd
Tamslay <matteo.glassey@heig-vd.ch>
2025-10-28 13:00:02 +0100

    maj index

---
+++ salle-de-jeux/index.php
@@ -0,0 +1,18 @@
+<?php
+require_once("includes/header.php");
+?>
+
+<main>
+    <h2>Bienvenue à LSBOWL!</h2>
+    <p>Réservez vos parties de <strong>LASER GAME</strong> et de <strong>BOWLING</strong> facilement. 
+    <br> Pour ceci, veuillez créer un compte ou vous connecter.</p>
+
+    <div class="buttons">
+        <a href="login.php" class="btn">Se connecter</a>
+        <a href="register.php" class="btn">Créer un compte</a>
+    </div>
+</main>
+
+<?php
+require_once("includes/footer.php");
+?>
