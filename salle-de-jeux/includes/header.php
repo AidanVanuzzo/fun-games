25e780ba91789f063f02a8a238a379775b2bdbd6
Tamslay <matteo.glassey@heig-vd.ch>
2025-10-28 12:58:51 +0100

    maj header

---
+++ salle-de-jeux/includes/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>LS BOWL</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <header>
+  
+    <div class="header-left">
+        <img src="assets/LOGO.png" alt="Logo Salle de Jeux" class="logo">
+
+        <h1>SALLE DE JEUX</h1>
+    </div>
+
+    <nav>
+        <a href="index.php">Accueil</a>
+        <a href="login.php">Connexion</a>
+        <a href="register.php">Créer un compte</a>
+    </nav>
+
+    </header>
