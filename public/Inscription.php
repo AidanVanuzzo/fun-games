778fe1b5b1a829456ff92fe8424667ba2fcb99b4
Aidan Vanuzzo <aidan.vanuzzo@gmail.com>
2025-11-24 12:51:29 +0100

    ajout du fichier Inscription.php

--- public/Inscription.php
+++ public/Inscription.php
@@ -22,5 +22,12 @@ require_once __DIR__ . '/../includes/header.php';
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="color-scheme" content="light dark">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
-    <title><?= $translations[$language]['form_title'] ?? "Choix de l'activité" ?></title>
+    <title><?= $translations[$language]['inscription_title'] ?? "Choix de l'activité" ?></title>
 </head>
+
+<body>
+    <div class="buttons">
+        <a href="InscriptionBowling.php" class="btn"><?= $translations[$language]['BOWLING'] ?></a>
+        <a href="InscriptionLG.php" class="btn"><?= $translations[$language]['LASER GAME'] ?></a>
+    </div>
+</body>
